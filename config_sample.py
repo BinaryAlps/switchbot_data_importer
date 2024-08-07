ceccac50dcbf0effcf97ffe95ef1c89e97c616aa
BinaryAlps <info@binary-alps.at>
2024-08-07 10:09:44 +0200

    [UPDATE] Script improvements + extension

--- config_sample.py
+++ config_sample.py
@@ -1,7 +1,12 @@
-config = {
+dbConfig = {
     'user': 'your_username',
     'password': 'your_password',
     'host': 'your_host',
     'database': 'your_database',
     'raise_on_warnings': True
+}
+
+config_params = {
+    'table_name': 'your_table_name',
+    'show_stats': True 
 }
